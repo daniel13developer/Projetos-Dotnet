71be1d46d92f6a14c105fefd5dd1c9543ec71029
daniel13developer <daniel.developer13@hotmail.com>
2024-11-08 14:44:22 -0300

    Estrutura inicial de exibição

--- SorteioMilhonario/Program.cs
+++ SorteioMilhonario/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class ProgamaSorteioMilhonario
+{
+    static void Main()
+    {
+        // iniciando varivel do sorteio
+        int quatidadeNumeros = 6;
+        int totalDeNumeros = 60;    
+
+        //exibir o resutado do sorteio
+        Console.WriteLine("Gerar bilhete:");       
+        
+        Console.WriteLine("*******************************************************");
+        Console.WriteLine("**                Bilhte da sorte!                   **");
+        Console.WriteLine("**   Soterio de " + quatidadeNumeros + " numeros                            **");
+        Console.WriteLine("**   De 1 a " + totalDeNumeros + "                                       **");
+        Console.WriteLine("**   Números sorteados                               **");
+        Console.WriteLine("**   XX XX XX XX XX XX                               **");
+        Console.WriteLine("*******************************************************");
+        
+    }
+}
